13627612b33aff4b95dddd6b3258d1f6c80c06a5
Karan Dhillon <Karanpal.dhillon@gmail.com>
2018-12-29 18:56:35 +0530

    initial commit

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    linters: {
+        '**/*.+(js|md|ts|css|sass|less|graphql|json)' : [
+            'eslint --fix',
+            'prettier --write',
+            'git add'
+        ],
+    },
+}
